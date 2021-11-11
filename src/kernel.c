a76995dfd4a30eed4893b4eea09ab7c933c0bc4c
ninjamar <70116773+ninjamar@users.noreply.github.com>
2021-11-11 08:29:10 +0800

    Update kernel.c

--- src/kernel.c
+++ src/kernel.c
@@ -2,12 +2,9 @@
 
 int kmain(){
   kclear();
-  char *a = "Hello, World!\n";
-  char *b = "This is ninjamar's kernel (nmk)\n";
-  char *c = "See the source code at https://github.com/ninjamar/nmk\n";
-  kprint(a);
-  kprint(b);
-  kprint(c);
+  char *msg = "Hello, World!\n";
+  kprint(msg);
+
   
   return 0;
 }
