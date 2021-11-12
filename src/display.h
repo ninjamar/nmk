1cf0fa9ad067de27de704b5c13c7dc0e94dfaf5c
ninjamar <70116773+ninjamar@users.noreply.github.com>
2021-11-12 16:11:37 +0800

    Update display.h

--- src/display.h
+++ src/display.h
@@ -7,33 +7,47 @@
 
 char *video = (char *)VGA_ADDRESS;
 
-int kprint(char *msg){
-  /*
-   * print {msg} to the display
-   */
-  int i = 0;
-  int j = 0;
+int y_pos = 0; // line
 
-  while (msg[j] != '\0'){
-    video[i] = msg[j];
-    video[i+1] = 0x07;
-    ++j;
-    i = i + 2;
-  }
-  return 0;
+int _len(char *msg){ // get length of string
+	int i = 0;
+	while (msg[i] != '\0'){
+		i++;
+	}
+	return i;
 }
 
-int kclear(){
-  /*
-   * clears the display
-   */
-  int i = 0;
-  int j = 0;
-  // 25 lines of 80 characters which take 2 bytes
-  while (j < 80*25*2){
-    video[j] = ' ';
-    video[j+1] = 0x07;
-    j = j + 2;
-  }
-  return 0;
+
+void kprint_x_y(char *msg,int x,int y){
+	int x_offset = (x % 2)+x; // x needs to be a multiple of 2 so we round up
+	int y_offset = ((y*80)*2 % 2)+(y*80*2); // (y*80)*2 = y = 1 = 80 * 2 = color bytes + round 
+	int offset = x_offset + y_offset;
+
+	char *o_vid = (char *)VGA_ADDRESS+offset; // the new video (starts at offset)
+	while (*msg != 0){ // copy msg to o_vid
+		*o_vid++ = *msg++;
+		*o_vid++ = 0x07;
+	}
+}
+
+void kclear(){
+	int s = 0; // string
+	int c = 1; // color
+	int i = 0;
+	while (i < 80*25*2){ // 80 chars * 25 lines * 2 bytes
+		video[s] = ' ';
+		video[c] = 0x07;
+		s = s + 2;
+		c = c + 2;
+		i++;
+	}
+}
+
+
+void kprint(char *msg){
+	// do text wrap in kprint_x_y
+	// handle newline
+	// handle textwrap
+	kprint_x_y(msg,0,y_pos); // always print a start of line
+	y_pos = y_pos + 1; // nextline
 }
